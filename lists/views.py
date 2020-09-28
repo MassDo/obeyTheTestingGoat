f101fd9693983806825715ed0f7e8a050314968f
MassDo <dorian_massoulier@hotmail.com>
2020-09-28 16:37:26 +0200

    refactoring refactoring test using client and template refactoring home_page view using template add lists to the installed apps settings

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-request = HttpRequest()
 
 def home_page(request):
-    response = HttpResponse()
-    response.content = '<html><title>To-Do</title></html>'
-    return response
+    return render(request, 'home.html')
