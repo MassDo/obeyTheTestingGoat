b3823a11ac53a46a64594d68c396f0569d4a5797
MassDo <dorian_massoulier@hotmail.com>
2020-09-28 15:26:12 +0200

    test fonctionnel vérification fonctionnelle d'un titre contenant 'To-Do' vérification fonctionnelle d'un champ de saisie contenant le placeholder 'Enter a to-do item' vérification fonctionnelle de l'ajout de la saisie utilisateur apres avoir pressé ENTER dans une ligne de tableau de la todo liste.

--- functional_tests.py
+++ functional_tests.py
@@ -15,22 +15,39 @@ class HomePageTest(unittest.TestCase):
         """
         self.brow.quit()
 
-    def test_home_page_title(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.brow.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.brow.title)
+        header = self.brow.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header)
 
-        self.fail('permet de faire échouer le test volontairementFini le test !')
         # She is invited to enter a to-do item straight away
+        inputbox = self.brow.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.brow.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(
+            any( "1: Buy peacock feathers" in row.text for row in rows )
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -45,6 +62,7 @@ class HomePageTest(unittest.TestCase):
 
 
         # Satisfied, she goes back to sleep
-
+        self.fail('permet de faire échouer le test volontairement. Fini le test !')
+        
 if __name__ == '__main__':
     unittest.main()
